0a411c18a337addc0d67b9363ef62d8e5260632d
NataBandeira <nata.wsd@gmail.com>
2021-02-07 09:26:52 -0300

    Fazer altercao na admin do usuario

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+
+    list_display = ('email', 'first_name')
+    list_filter = ()
+    list_display_links =('email',)
+    search_fields = ('email', 'first_name')
+
+
+
+admin.site.register(Usuario, UsuarioAdmin)
